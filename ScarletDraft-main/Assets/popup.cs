a3095f249a3c23bc33a06d4d70439d59986eb2a8
anu-shka-singh <anushkasingh2811@gmail.com>
2022-09-10 16:46:17 +0530

    added sword popup

--- ScarletDraft-main/Assets/popup.cs
+++ ScarletDraft-main/Assets/popup.cs
@@ -6,14 +6,20 @@ using UnityEngine.UI;
 public class popup : MonoBehaviour
 {
 
+    
     [SerializeField]
-    private Image clue2Img;
+    private GameObject clue1;
+
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            clue2Img.enabled = true;
+            ShowCursor();
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                HideCursor();
+            }
         }
     }
 
@@ -21,7 +27,27 @@ public class popup : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            clue2Img.enabled = false;
+            
+            HideCursor();
         }
+    }
+
+
+    public void ShowCursor()
+    {
+
+        clue1.SetActive(true);
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void HideCursor()
+    {
+
+        clue1.SetActive(false);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
