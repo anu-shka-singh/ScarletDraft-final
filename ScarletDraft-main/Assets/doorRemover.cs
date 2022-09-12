3fca5a1f02c098639c878152a135a1f8f304bae3
anu-shka-singh <anushkasingh2811@gmail.com>
2022-09-12 17:10:36 +0530

    door script added

---
+++ ScarletDraft-main/Assets/doorRemover.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class doorRemover : MonoBehaviour
+    
+{
+    public GameObject key1;
+    public GameObject key2;
+    public GameObject key3;
+       public GameObject door;
+
+   //public Transform target;
+    public float t;
+
+    //public Transform sceptor;
+    //public sceptormove sceptorMove;
+    public AudioSource Correct;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.gameObject.name == "Player" && !key1.activeSelf && !key2.activeSelf && !key3.activeSelf)
+        {
+            Correct.Play();
+            //sceptorMove.sceptor();
+            Destroy(gameObject);
+            door.SetActive(false);
+
+
+            
+          // StartCoroutine(ExecuteAfterTime());
+        }
+    }
+
+}
